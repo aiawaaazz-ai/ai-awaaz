f9f770caf1752fa88a6292ac7cc4f04cdb9f11e3
mohitmadras <mohitmadras@gmail.com>
2025-09-21 20:05:55 +0530

    updated name

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
+import {About} from './features/about/about';
 
 export const routes: Routes = [
   {
     path: 'about',
-    loadComponent: () => import('./features/about/about').then((m) => m.About),
+    component: About,
     title: 'About',
   },
   {
